886a4ea1c072629286c6dc39262102b5fd03199e
Niranjan Shah <niranjanshah474@gmail.com>
2023-09-20 15:10:11 +1000

    request body done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -99,3 +100,25 @@ async def get_user_item(user_id: int, item_id: str, q: str | None = None, short:
     if not short:
         item.update({"description": "Lorem ipsum"})
     return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result ={"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q" : q})
+    return result

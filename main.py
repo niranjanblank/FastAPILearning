71a679f877c730483484edb066ee73d2e5296b41
Niranjan Shah <niranjanshah474@gmail.com>
2023-09-25 23:39:02 +1000

    Numeric validation added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel
@@ -108,6 +108,7 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+
 @app.post("/items")
 async def create_item(item: Item):
     item_dict = item.model_dump()
@@ -116,23 +117,34 @@ async def create_item(item: Item):
         item_dict.update({'price_with_tax': price_with_tax})
     return item_dict
 
+
 @app.put("/items/{item_id}")
 async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
-    result ={"item_id": item_id, **item.model_dump()}
+    result = {"item_id": item_id, **item.model_dump()}
     if q:
-        result.update({"q" : q})
+        result.update({"q": q})
     return result
 
 
 @app.get("/items")
-async def read_items(q: list[str] = Query(...,min_length=4,max_length=10, alias="item-query")):
-    results = {"items":fake_items_db}
+async def read_items(q: list[str] = Query(..., min_length=4, max_length=10, alias="item-query")):
+    results = {"items": fake_items_db}
     if q:
         results.update({"q": q})
     return results
+
 
 @app.get('/items_hidden')
 async def hidden_query_route(hidden_query: str = Query(None, include_in_schema=False)):
     if hidden_query:
         return {"hidden_query": hidden_query}
     return {"hidden_query": "hidden_query not found"}
+
+
+@app.get("/items_validation/{item_id}")
+async def read_items_validation(*, item_id: int = Path(..., title="The ID of the item to get", ge=10),
+                                q: str = Query("Hello")):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
